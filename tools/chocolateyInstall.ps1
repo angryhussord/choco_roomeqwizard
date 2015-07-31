ab5202e5260219a88d8adc4ca0443815763daece
angryhussord <angryhussord@outlook.com>
2015-07-31 15:59:10 -0700

    initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'roomeqwizard' 
+$installerType = 'exe' 
+$url = 'http://www.roomeqwizard.com/installers/REW_windows_5_12.exe' 
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
