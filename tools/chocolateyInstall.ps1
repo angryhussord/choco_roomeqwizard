0f867b3234d3e9b58a68e9c8e0347feb365409ed
Patrick Hufford <pahuff@microsoft.com>
2015-08-26 16:35:26 -0700

    Updated silent install switches
    New silent install switches for 5.13

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,6 @@
 ﻿$packageName = 'roomeqwizard' 
 $installerType = 'exe' 
 $url = 'http://www.roomeqwizard.com/installers/REW_windows_5_13.exe' 
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+$silentArgs = '-q' 
 $validExitCodes = @(0) 
-
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
