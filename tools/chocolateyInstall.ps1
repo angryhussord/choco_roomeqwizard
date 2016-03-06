8ca54aa053624a794acb8a264c2bdfa232797759
Patrick Hufford <pahuff@microsoft.com>
2016-03-06 08:01:32 -0800

    updates for 5.14 and new chocolatey packaging guidelines

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = 'roomeqwizard' 
-$installerType = 'exe' 
-$url = 'http://www.roomeqwizard.com/installers/REW_windows_5_13.exe' 
-$silentArgs = '-q' 
-$validExitCodes = @(0) 
+﻿$packageName = 'roomeqwizard'
+$installerType = 'exe'
+$url = 'http://www.roomeqwizard.com/installers/REW_windows_5_14.exe'
+$silentArgs = '-q'
+$validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
