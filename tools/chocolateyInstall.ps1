554b48950c9bb6c578fd75aab5b5e4bed1326361
Patrick Hufford <pahuff@microsoft.com>
2017-01-25 13:37:29 -0800

    5.18 update

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,21 @@
-﻿$packageName = 'roomeqwizard' 
-$installerType = 'exe' 
-$url = 'http://www.roomeqwizard.com/installers/REW_windows_5_16.exe' 
-$silentArgs = '-q' 
-$validExitCodes = @(0) 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'roomeqwizard'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
+$installerType = 'EXE'
+$url = 'http://www.roomeqwizard.com/installers/REW_windows_5_18.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = $installerType
+  url           = $url
+  url64bit      = $url64
+  silentArgs    = '-q'
+  validExitCodes= @(0)
+  softwareName  = 'roomeqwizard*'
+  checksum      = '2AFC9DE36A2CEA502C17D9FD914592CC'
+  checksumType  = 'sha256'
+};
+
+Install-ChocolateyPackage @packageArgs;
